1871274fed005b2be6f646bd99d4aa1af7659c7d
Azim Shamuratov <lennon645@yandex.com>
2022-09-07 10:50:33 +0500

    Task #2. Key Points - 3

--- src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
@@ -1,6 +1,10 @@
 package lang.print.gaps.task2;
 
 public class AdvancedNamingConvention {
+    private static final int ADULT_AGE = 18;
+
+    private int age;
+
     void callToFriend() {
         //some code
     }
